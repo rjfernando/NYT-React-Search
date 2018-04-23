207fe4a84619563e43de41f1b93008bec0ba72eb
Rommel Fernando <rommelfernando@Rommels-MacBook-Pro.local>
2018-04-22 22:08:11 -0700

    update package.json

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const NavBar = () =>
   <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-
-    <a className="navbar-brand" href="/search">NYT-React-Search</a>
-
+   <a className="navbar-brand" href="/search">NYT-React-Search</a>
     <ul className="navbar-nav">
       <li className="nav-item">
         <a className="nav-link" href="/search">Search Articles</a>
